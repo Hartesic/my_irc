9f887a9956ae04c21c36d9e1c5d1da295bede9d3
Ambroise Christea <ambroise.christea@cloudwatt.com>
2014-07-16 17:27:18 +0200

    Partie obligatoire terminée + bonus (temp)

--- app.js
+++ app.js
@@ -10,36 +10,133 @@ app.get('/', function(req, res){
 	res.sendfile('index.html');
 });
 
-var chans = new Array();
-var users = new Object();
-chans.push('test');
-users['test'] = 'test';
+var user_sockets = new Object();
+var chans = new Object();
+user_sockets['#test'] = new Object();
+user_sockets['#azer'] = new Object();
+chans['#test'] = new Array();
+chans['#azer'] = new Array();
+
+function object_to_array(object, str)
+{
+	if (typeof string == 'undefined')
+		str = '';
+	array = new Array();
+	for (property in object){
+		if (property.search(str) > -1)
+			array.push(property);
+	}
+	return array;
+}
 
 io.on('connection', function(socket){
 	var user = new Object();
+	user.color = '#000';
 	socket.on('join', function(infos){
 //		console.log('<' + typeof infos + '>' + infos);
-		if (chans.indexOf(infos.chan) > -1){
-			if (typeof users[infos.name] == 'undefined' || users[infos.name] != infos.chan){
-				user.name = infos.name;
-				user.chan = infos.chan;
-				socket.join(infos.chan);
-				socket.emit('ok', user);
+		if (typeof infos == 'undefined')
+			return socket.emit('nope', 'both', 'Don\'t edit the code, noob! Reload the page, now.');
+		if (typeof infos.chan == 'undefined' || infos.chan == '')
+			return socket.emit('nope', 'chan-noob', 'Don\'t edit the code, noob! Reload the page, now.');
+		if (typeof infos.name == 'undefined' || infos.name == '')
+			return socket.emit('nope', 'name-noob', 'Don\'t edit the code, noob! Reload the page, now.');
+		if (typeof chans[infos.chan] != 'undefined'){
+			if (chans[infos.chan].indexOf(infos.name) == -1){
+				user.name = infos.name.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+				user.chan = infos.chan.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+				socket.join(user.chan);
+				chans[user.chan].push(user.name);
+				user_sockets[user.chan][user.name] = socket;
+				io.to(user.chan).emit('log', 'joined the channel', user.name)
+				return socket.emit('ok', user);
 			} else
-				socket.emit('nope', 'name', 'This name is already registered in this channel');
+				return socket.emit('nope', 'name', 'This name is already registered in this channel');
 		} else
-			socket.emit('nope', 'chan', 'This channel does not exist');
+			return socket.emit('nope', 'chan', 'This channel does not exist');
 	});
 	socket.on('message', function(msg){
-//		console.log('message: ' + msg);
-		io.to(user.chan).emit('message', msg);
+		if (/^\/nick .+/.test(msg)){
+			var new_name = msg.match(/^\/nick (.+)/)[1].replace(/</g, '&lt;').replace(/>/g, '&gt;');
+			if (chans[user.chan].indexOf(new_name) == -1){
+				var old_name = user.name;
+				user.name = new_name;
+				chans[user.chan][chans[user.chan].indexOf(old_name)] = user.name
+				user_sockets[user.chan][user.name] = user_sockets[user.chan][old_name];
+				delete user_sockets[user.chan][old_name];
+				return io.to(user.chan).emit('system', old_name + '</strong> is now known as <strong>' + user.name);
+			} else
+				return socket.emit('system', 'A user called <strong>' + new_name + '</strong> is already connected in this channel', 'system-red');
+		} else if (/^\/list( (.+))?/.test(msg)){
+			if (/^\/list$/.test(msg))
+				return socket.emit('channels', object_to_array(chans));
+			else if (/^\/list (.+)/.test(msg))
+				return socket.emit('channels', object_to_array(chans, msg.match(/^\/list (.+)/)[1]));
+		} else if (/^\/join .+/.test(msg)){
+			var new_chan = msg.match(/^\/join (.+)/)[1].replace(/</g, '&lt;').replace(/>/g, '&gt;');
+			var	name = user.name;
+			if (/^\/join .+ .+/.test(msg))
+				name = msg.match(/^\/join (.+)( (.+))/)[3].replace(/</g, '&lt;').replace(/>/g, '&gt;');
+			if (chans[new_chan] != 'undefined'){
+				if (chans[new_chan].indexOf(name) == -1){
+					var old_chan = user.chan;
+					var old_name = user.name;
+					user.chan = new_chan;
+					user.name = name;
+					socket.leave(old_chan);
+					chans[old_chan].splice(chans[old_chan].indexOf(old_name), 1);
+					delete user_sockets[old_chan][old_name];
+					io.to(old_chan).emit('chan', old_name, 'left');
+					socket.join(user.chan);
+					chans[user.chan].push(user.name);
+					user_sockets[user.chan][user.name] = socket;
+					io.to(user.chan).emit('chan', user.name, 'joined');
+					return socket.emit('relog', user);
+				} else
+					return socket.emit('system', 'A user called <strong>' + name + '</strong> is already connected in the channel "' + new_chan + '"', 'system-red');
+			} else
+				return socket.emit('system', 'Channel "' + new_chan + '" does not exist', 'system-red');
+		} else if (/^\/part/.test(msg)){
+			name = user.name;
+			socket.leave(user.chan);
+			chans[user.chan].splice(chans[user.chan].indexOf(name), 1);
+			delete user_sockets[user.chan][user.name];
+			io.to(user.chan).emit('chan', name, 'left');
+			user.name = '';
+			user.chan = '';
+			return socket.emit('logout');
+		} else if (/^\/users/.test(msg)){
+			return socket.emit('users', chans[user.chan]);
+		} else if (/^\/msg (.+) (.+)/.test(msg)){
+			var to = msg.match(/^\/msg (.+) (.+)/)[1].replace(/</g, '&lt;').replace(/>/g, '&gt;');
+			if (chans[user.chan].indexOf(to) > -1){
+				var msg = msg.match(/^\/msg (.+) (.+)/)[2].replace(/</g, '&lt;').replace(/>/g, '&gt;');
+				user_sockets[user.chan][to].emit('r-pm', user.name, msg);
+				return socket.emit('s-pm', to, msg);
+			}
+			return socket.emit('system', 'User <strong>' + to + '</strong> is unknown on this channel', 'system-red');
+		} else if (/^\/color \#[a-f0-9]{3}$/i.test(msg)){
+			user.color = msg.match(/^\/color (\#[a-f0-9]{3})/i)[1];
+			return socket.emit('system', 'Your color has been changed to "<span style="color: ' + user.color + ';">' + user.color + '</span>"');
+		} else if (/^\/help/.test(msg)) {
+			if (/^\/help$/.test(msg))
+				return socket.emit('help', commands)
+			else if (/^\/help .+/.test(msg)){
+				if (typeof command = commands[msg.match(/^\/help (.+)/)] != 'undefined')
+					return socket.emit('help', commands[command]);
+			}
+			return ;
+		} else
+			return io.to(user.chan).emit('message', user.name, user.color, msg.replace(/</g, '&lt;').replace(/>/g, '&gt;'));
 	})
-	socket.on('disconnect', function(user){
+	socket.on('disconnect', function(){
 //		console.log(user + ' has disconnected');
-		io.to(user.chan).emit('disconnected', user);
+		if (typeof user.chan != 'undefined' && user.chan != '' && typeof user.name != 'undefined' && user.name != '' && chan[user.chan].indexOf(user.name) > -1){
+			chans[user.chan].splice(chans[user.chan].indexOf(user.name), 1);
+			io.to(user.chan).emit('log', 'disconnected', user.name);
+		}
 	})
 })
 
 http.listen(1337, function(){
 	console.log('listening on *:1337');
-})
+});
---
+++ public/js/client.js
@@ -0,0 +1,101 @@
+var socket = io();
+var user;
+
+function adjust_scroll(){
+  $('#messages-list').scrollTop($('.message').length * $('.message').outerHeight(true));
+}
+
+$('#join-form').submit(function(){
+  $('#alert-error').css('display', 'none');
+  $('#chan-group').removeClass('has-error');
+  $('#name-group').removeClass('has-error');
+  socket.emit('join', {
+    name: $('#name').val(),
+    chan: $('#chan').val()
+  });
+  return false;
+});
+$('#message-form').submit(function(){
+  if ($('#msg').length > 0 && $('#msg').val() != '') {
+    socket.emit('message', $('#msg').val());
+    $('#msg').val('');
+    return false;
+  }
+});
+socket.on('ok', function(infos){
+  $('#join').fadeOut();
+  $('#messages').fadeIn();
+  $('#chan_name').text(infos.chan);
+  $('#messages-list').append($('<li class="system">').text('Welcome on the channel "' + infos.chan + '", <strong>' + infos.name + '</strong>. Be polite and enjoy your stay. :)'));
+  adjust_scroll();
+  user = infos;
+});
+socket.on('relog', function(infos){
+  $('#chan_name').text(infos.chan);
+  $('#messages-list').empty();
+  $('#messages-list').append($('<li class="system">').text('Welcome on the channel "' + infos.chan + '", <strong>' + infos.name + '</strong>. Be polite and enjoy your stay. :)'));
+  adjust_scroll();
+  user = infos;
+});
+socket.on('logout', function(infos){
+  $('#join').fadeIn();
+  $('#messages').fadeOut();
+  $('#chan_name').text('');
+})
+socket.on('nope', function(input, text){
+  if (['chan-noob', 'name-noob', 'both'].indexOf(input) > -1)
+    $('#alert-error').fadeIn().text(text)
+  if (input == 'chan' || input == 'both')
+    $('#chan-group').addClass('has-error');
+  else if (input == 'name' || input == 'both')
+    $('#name-group').addClass('has-error');
+  $('#alert-error').fadeIn().text(text);
+});
+socket.on('message', function(usr, color, msg){
+  $('#messages-list').append(($('<li class="message">').text(': ' + msg)).prepend($('<strong class="usr" style="color: ' + color + '">').text(usr)));
+  adjust_scroll();
+});
+socket.on('r-pm', function(from, msg){
+  $('#messages-list').append($('<li class="system-blue">').text('You have received a private message from <strong>' + from.name + '</strong>:'));
+  $('#messages-list').append($('<li class="message">').text(msg));
+  $('#messages-list').append($('<li class="system-blue">').text('------------------------------------------'));
+  adjust_scroll();
+});
+socket.on('s-pm', function(to, msg){
+  $('#messages-list').append($('<li class="system-blue">').text('To <strong>' + to + '</strong>:'));
+  $('#messages-list').append($('<li class="message">').text(msg));
+  $('#messages-list').append($('<li class="system-blue">').text('------------------------------------------'));
+  adjust_scroll();
+});
+socket.on('log', function(log, usr){
+  $('#messages-list').append(($('<li class="system">').text(' has ' + log)).prepend($('<strong>').text(usr)));
+  adjust_scroll();
+});
+socket.on('system', function(msg, status){
+  if (typeof status == 'undefined')
+    status = 'system';
+  $('#messages-list').append($('<li class="' + status + '">').text(msg));
+  adjust_scroll();
+});
+socket.on('chan', function(usr, chan){
+  $('#messages-list').append(($('<li class="system">').text(' has ' + chan + ' the channel')).prepend($('<strong>').text(usr)));
+  adjust_scroll();
+});
+socket.on('users', function(users){
+  $('#messages-list').append($('<li class="system-green">').text('------------------------------------------'));
+  $('#messages-list').append($('<li class="system-green">').text('There\'re ' + users.length + ' users on this channel:'));
+  for (user in users){
+    $('#messages-list').append($('<li class="system-green"><strong>').text(users[user]));
+  }
+  $('#messages-list').append($('<li class="system-green">').text('------------------------------------------'));
+  adjust_scroll();
+});
+socket.on('channels', function(channels){
+  $('#messages-list').append($('<li class="system-green">').text('------------------------------------------'));
+  $('#messages-list').append($('<li class="system-green">').text('There\'re ' + channels.length + ' channels on this server:'));
+  for (channel in channels){
+    $('#messages-list').append($('<li class="system-green"><strong>').text(channels[channel]));
+  }
+  $('#messages-list').append($('<li class="system-green">').text('------------------------------------------'));
+  adjust_scroll();
+});
